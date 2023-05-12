6677b80cdc0ff12a074ef5e8c54ce7f5f73eea03
CiarenceW <112902813+CiarenceW@users.noreply.github.com>
2023-05-12 16:12:50 +0200

    Added fix for Bomb Bot
    Actually haven't test if it works, if it does, I'm a god.

---
+++ Src/Plugins/RobotTweaks.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Receiver2;
+using UnityEngine;
+using System.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiarenceUnbelievableModifications
+{
+    public static class GunTweaks
+    {
+        //only fixes the main_spring on the deagles for now.
+
+        [HarmonyPatch(typeof(ReceiverCoreScript), "Awake")]
+        [HarmonyPostfix]
+        private static void PatchCoreAwake(ref ReceiverCoreScript __instance)
+        {
+			var bomb_bot = __instance.enemy_prefabs.bomb_bot.GetComponent<BombBotScript>();
+			bomb_bot.voice_filter = "event:/TextToSpeech/TextToSpeech - bomb bot";
+        }
+    }
+}
