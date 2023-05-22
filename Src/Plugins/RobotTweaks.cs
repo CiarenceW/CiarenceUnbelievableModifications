f6738821d6401f26021af2b03ba988fc84614fc9
CiarenceW <corentin.pansart@gmail.com>
2023-05-22 21:05:45 +0200

    Added libs to make things less shit for me. Fixed whatever fucking bullshit I did when I tried to merge the fucking branches instead of syncing like a normal fucking human being

--- Src/Plugins/RobotTweaks.cs
+++ Src/Plugins/RobotTweaks.cs
@@ -14,11 +14,11 @@ namespace CiarenceUnbelievableModifications
 {
     public static class RobotTweaks
     {
+        public static Color tripmine_beam_colour;
+
         public static Color colour_normal;
         public static Color colour_alert;
         public static Color colour_alert_shooting;
-
-        public static Color tripmine_beam_colour;
 
         public static Color colour_idle_turret;
         public static Color colour_alert_turret;
@@ -58,7 +58,8 @@ namespace CiarenceUnbelievableModifications
         {
             var bomb_bot = ReceiverCoreScript.Instance().enemy_prefabs.bomb_bot.GetComponent<BombBotScript>();
 
-            //this doesn't currently do anything, I think it used to talk and stuff and it was cool but it doesn't anymore. It just beeps. Capitalism. Also, if you don't change it to a valid event like the one below, it used to soft-crash (is that a thing)
+            //this doesn't currently do anything, I think it used to talk and stuff and it was cool but it doesn't anymore. It just beeps. Capitalism.
+            //Also, if you don't change it to a valid event like the one below, it used to soft-crash (is that a thing)
             bomb_bot.voice_filter = "event:/TextToSpeech/TextToSpeech - bomb bot";
 
             //AccessTools.Field(typeof(LightPart), "passive_color").SetValue(__instance.enemy_prefabs.shock_drone.GetComponent<ShockDrone>().light_part, new Color(1f, 0f, 1f, 1f));
@@ -136,67 +137,95 @@ namespace CiarenceUnbelievableModifications
         //can you tell from this that I'm going insane?
         private static void PatchLightPartUpdate(ref LightPart __instance)
         {
+            //let's assume, for the sake of the arguement that, hypothetically, the object that the LightPart is attached to is a Shock Drone.
             var light_color = typeof(LightPart).GetField("light_color", BindingFlags.Instance | BindingFlags.NonPublic);
             var shockDroneScript = __instance.transform.parent.parent.GetComponent<ShockDrone>();
             if (shockDroneScript != null)
             {
                 var state = (ShockDroneState)typeof(ShockDrone).GetField("state", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(shockDroneScript);
 
-                if (verbose) Debug.LogFormat("State is {0}", state);
                 switch (state)
                 {
                     case ShockDroneState.Idle:
-                        light_color.SetValue(__instance, colour_normal);
+                        light_color.SetValue(__instance, (campaign_has_override) ? colour_override_idle : colour_idle_drone);
                         return;
                     case ShockDroneState.Alert:
                     case ShockDroneState.TrackPlayer:
                     case ShockDroneState.Ramming:
-                        light_color.SetValue(__instance, colour_alert);
+                        light_color.SetValue(__instance, (campaign_has_override) ? colour_override_alert : colour_alert_drone);
                         return;
                     case ShockDroneState.Attacking:
-                        light_color.SetValue(__instance, colour_alert_shooting);
+                        light_color.SetValue(__instance, (campaign_has_override) ? colour_override_attack : colour_attacking_drone);
                         return;
                     case ShockDroneState.Standby:
-                        var passive_colour = colour_normal;
+                        var passive_colour = colour_idle_drone;
                         passive_colour.a = 0f;
-                        light_color.SetValue(__instance, passive_colour);
+                        var passive_colour_override = colour_override_idle;
+                        passive_colour_override.a = 0f;
+                        light_color.SetValue(__instance, (campaign_has_override) ? passive_colour_override : passive_colour);
                         return;
                     default:
-                        light_color.SetValue(__instance, colour_normal);
+                        light_color.SetValue(__instance, (campaign_has_override) ? colour_override_idle : colour_idle_drone);
                         return;
                 }
             }
+
+            //if the thing that the LightPart is attached isn't a shockdrone, we'll assume that it's a security cam.
             var securityCameraScript = __instance.transform.parent.parent.GetComponent<SecurityCamera>();
             if (securityCameraScript != null)
             {
                 var state = (SecurityCameraState)typeof(SecurityCamera).GetField("state", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(securityCameraScript);
 
-                if (verbose) Debug.LogFormat("State is {0}", state);
                 switch (state)
                 {
                     case SecurityCameraState.Idle:
-                        light_color.SetValue(__instance, colour_normal);
+                        light_color.SetValue(__instance, (campaign_has_override) ? colour_override_idle : colour_idle_camera);
                         return;
                     case SecurityCameraState.Alert:
-                        light_color.SetValue(__instance, colour_alert);
+                        light_color.SetValue(__instance, (campaign_has_override) ? colour_override_alert : colour_alert_camera);
                         return;
                     case SecurityCameraState.Alarm:
-                        light_color.SetValue(__instance, colour_alert_shooting);
+                        light_color.SetValue(__instance, (campaign_has_override) ? colour_override_attack : colour_alarming_camera);
                         return;
                     case SecurityCameraState.Off:
-                        var passive_colour = colour_normal;
+                        var passive_colour = colour_idle_camera;
                         passive_colour.a = 0f;
-                        light_color.SetValue(__instance, passive_colour);
+                        var passive_colour_override = colour_override_idle;
+                        passive_colour_override.a = 0f;
+                        light_color.SetValue(__instance, (campaign_has_override) ? passive_colour_override : passive_colour);
                         return;
                     default:
-                        light_color.SetValue(__instance, colour_normal);
+                        light_color.SetValue(__instance, (campaign_has_override) ? colour_override_idle : colour_idle_camera);
                         return;
                 }
             }
             if (verbose) Debug.LogError("fucksguhuogsrgksgsfsgehlfse");
-            //var light_colour = (Color)AccessTools.Field(typeof(LightPart), "light_color").GetValue(__instance);
-            //light_colour = new Color(0f, 1f, 1f, 1f);
-            //Debug.Log("THIS PISS OF SHIT DOESN'T FUKING WORKY!!!!!");
+        }
+
+        //for the turrets' camera colours when in TC, you can't change from which field the thing is read during runtime I think with Transpilers.
+        public static void OnPlayerInitialize(ReceiverEventTypeVoid ev)
+        {
+            if (ReceiverCoreScript.Instance().game_mode.GetGameMode() != GameMode.RankingCampaign) return;
+
+            FieldInfo campaign_name = typeof(RankingProgressionGameMode).GetField("campaign_name", BindingFlags.Instance | BindingFlags.NonPublic);
+            RankingProgressionGameMode rpgm = ReceiverCoreScript.Instance().game_mode.GetComponent<RankingProgressionGameMode>();
+            if (verbose) Debug.Log(campaign_name.GetValue(rpgm));
+            if ((string)campaign_name.GetValue(rpgm) != "bozo_torture_campaign")
+            {
+                campaign_has_override = false;
+                colour_normal = colour_idle_turret;
+                colour_alert = colour_alert_turret;
+                colour_alert_shooting = colour_attacking_turret;
+                return;
+            }
+            campaign_has_override = true;
+            colour_override_idle = tc_colour_idle;
+            colour_override_alert = tc_colour_alert;
+            colour_override_attack = tc_colour_attack;
+            colour_normal = colour_override_idle;
+            colour_alert = colour_override_alert;
+            colour_alert_shooting = colour_override_attack;
+            if (verbose) Debug.Log("player is bozo");
         }
 
         public static void Discolights()
